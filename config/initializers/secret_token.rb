7cca41a27b6a4908d695fe5ed6fdb2a627a2dedd
davcro <davcro@me.com>
2012-08-25 14:20:00 +0200

    first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EfarDispatch::Application.config.secret_token = 'a9ade3ca84087ebfc48164c759c424359abdc83c0333ba166be8194701a1d4d505328db3438c96b443a3111a707444a99c22aa952bd03baea33c2c8e5fbd95b3'
