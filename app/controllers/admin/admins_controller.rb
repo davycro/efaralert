74a089b20dd4efab850678ba8e3202bec907a271
davcro <davcro@me.com>
2012-10-17 10:53:14 -0600

    styled admin index

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,9 +1,31 @@
 class Admin::AdminsController < ApplicationController
-  layout 'efar_chrome'
+  layout 'efar_admin'
 
   before_filter :require_admin_login
 
   def index
+    @admins = Admin.all
+  end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def show
+    @admin = Admin.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 
