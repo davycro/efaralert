2b1a537989c34cff92d7518b77e80071bcd5cda9
davcro <davcro@me.com>
2013-02-04 18:48:21 +0200

    init testing for efars

---
+++ test/unit/efar_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: efars
+#
+#  id                  :integer          not null, primary key
+#  full_name           :string(255)      not null
+#  community_center_id :integer          not null
+#
+
+require 'test_helper'
+
+class EfarTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "has many locations" do
+    david = efars(:david)
+    assert david.locations.present?
+  end
+end
