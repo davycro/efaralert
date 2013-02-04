774e0b6832395e1c2a3e10ffbe23973576109238
davcro <davcro@me.com>
2013-02-04 18:02:36 +0200

    updated efar model, needs testing

--- app/models/efar.rb
+++ app/models/efar.rb
@@ -11,30 +11,15 @@
 class Efar < ActiveRecord::Base
 
   # Individual Attributes
-  attr_accessible :first_name, :surname, :contact_number, :community_center_id
-
-  # Location Attributes
-  attr_accessible :street, :suburb, :postal_code, :city, :province, :country
-
-  # Geocoding Attributes
-  attr_accessible :lat, :lng, :location_type
-
-  # Personal Attributes
-  attr_accessible :first_language
+  attr_accessible :full_name, :community_center_id
 
   PER_PAGE = 50
 
-  validates :surname, :street, :city, :country, :community_center_id, 
-    :contact_number, :presence => true
+  validates :full_name, :community_center_id, :presence => true
 
   belongs_to :community_center
   has_many :dispatch_messages
-
-
-  # scopes
-  scope :valid_location,
-    where("location_type not in (?) and location_type is not null", 
-      %w{sublocality postal_code administrative_area_level_1 locality})
+  has_many :locations, :class_name => 'EfarLocation'
 
   def self.all_for_page(page)  
     page ||= 0
@@ -43,52 +28,17 @@ class Efar < ActiveRecord::Base
     return self.limit(per_page).offset(per_page*page).all
   end
 
-  def full_name
-    "#{self.first_name} #{self.surname}"
-  end
-
-  geocoded_by :geocode_search_address,
-    :latitude => :lat, :longitude => :lng do |obj, results|
-    if geo = results.first
-      obj.lat               = geo.latitude
-      obj.lng               = geo.longitude
-      obj.location_type     = geo.types.first
-    end
-  end 
-
-  def geocode_search_address
-    [street, suburb, city, postal_code, province, country].compact.join(", ")
-  end
-
-  def contact_number_formatted_for_clickatell
-    # TODO make this error proof
-    # quick shim, add +27 to the number
-    "#{self.contact_number}"
-  end
-
   def head_efars
     @head_efars ||= self.community_center.head_efars
   end
 
-  def formatted_address
-    geocode_search_address
-  end
-
   def as_json(options = {})
-    super(:methods => [:lat, :lng, :full_name, :formatted_address])
-  end
-
-  def set_defaults
-    if self.new_record?
-      self.city           = "Cape Town"
-      self.province       = "Western Cape"
-      self.country        = "South Africa"
-      self.first_language = "English"
-    end
+    super(:methods => [:locations])
   end
 
   def send_text_message(message)
-    return SMS_API.send_message(self.contact_number, message)
+    # TODO fix this
+    # return SMS_API.send_message(self.contact_number, message)
   end
 
 end
--- app/models/efar_location.rb
+++ app/models/efar_location.rb
@@ -13,4 +13,33 @@
 #
 
 class EfarLocation < ActiveRecord::Base
+
+  #
+  # Time of day when location is occupied
+  # eg daytime, nighttime, anytime
+  attr_accessible :occupied_at
+
+  # geocoding attributes
+  attr_accessible :given_address, :formatted_address, :lat, :lng,
+    :location_type
+
+  validates :given_address, :efar_id, :presence => true
+  after_validation :geocode
+
+  geocoded_by :given_address,
+    :latitude => :lat, :longitude => :lng do |obj, results|
+    if geo = results.first
+      obj.lat           = geo.latitude
+      obj.lng           = geo.longitude
+      obj.location_type = geo.types.first
+    end
+  end
+
+  scope :valid_location,
+    where("location_type not in (?) and location_type is not null", 
+      %w{sublocality postal_code administrative_area_level_1 locality})
+
+  belongs_to :efar
+
+
 end
